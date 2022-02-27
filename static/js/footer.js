9c85266fc5a68dbb54a4fe13775ba070bd9dfad3
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-02-27 03:32:18 +0200

    Update Footer

---
+++ static/js/footer.js
@@ -0,0 +1,70 @@
+document.write(`
+		<div class="container">
+			<div class="row justify-content-start">
+				<div class="row col-8">
+					<!-- Social Media -->
+					<div class="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-8">
+						<ul class="list-unstyled navbar">
+							<li>
+								<a href="mailto:abdeltwab.m.fakhry@protonmail.com" target="_blank">
+									<i class="fas fa-envelope fs-2 text-dark"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/AbdeltwabMF" target="_blank">
+									<i class="fab fa-github fs-2 text-dark"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://linkedin.com/in/AbdeltwabMF" target="_blank">
+									<i class="fab fa-linkedin fs-2 text-dark"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://t.me/AbdeltwabMF" target="_blank">
+									<i class="fab fa-telegram fs-2 text-dark"></i>
+								</a>
+							</li>
+						</ul>
+						<hr>
+					</div>
+
+					<!-- Made with Love and Faith -->
+					<div class="col-12">
+						Created in
+						<a href="https://www.gnu.org/"><img style="width: 24px;" src="/assets/images/uses/os/gnu.svg" alt="GNU" class="sm-icon-img"></a>
+						&sol;
+						<a href="https://github.com/torvalds/linux"><img style="width: 24px;" src="/assets/images/uses/os/linux.svg" alt="Linux" class="sm-icon-img"></a>
+						&amp;
+						<a href="https://github.com/neovim/neovim"><img style="width: 24px;" src="/assets/images/uses/app/vim.svg" alt="Vim Icon" class="sm-icon-img"></a>
+						with
+						<i class="fas fa-heart fs-4 text-danger"> </i>
+					</div>
+
+					<!-- Inspiration -->
+					<div class="col-12">
+						The theme is heavily inspired by <a class="text-decoration-none" href="https://github.com/morhetz/gruvbox">gruvbox</a>.
+					</div>
+
+					<div class="col-12">
+						Powered by <a class="text-decoration-none" href="https://getbootstrap.com/"><i class="fab fa-bootstrap fs-3 text-primary"></i> Bootstrap</a>
+					</div>
+
+					<!-- Copyleft -->
+					<div class="col-12">
+						&copy;
+						<span>2021, 2022</span>
+						<span>Abd El-Twab M. Fakhry</span>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- live 2d widget -->
+		<canvas
+			id="live2dcanvas"
+			height="250"
+			width="150"
+			class="position-fixed bottom-0 start-0"
+			>
+		</canvas>`);
