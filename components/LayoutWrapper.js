0b29a10fc8c8ecb5ad66ab16e3ebe7db778f9345
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-31 01:53:51 +0200

    Fix logo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -10,22 +10,22 @@ import ThemeSwitch from './ThemeSwitch'
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-5">
+      <div className='flex flex-col justify-between h-screen'>
+        <header className='flex items-center justify-between py-5'>
           <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <Link href='/' aria-label={siteMetadata.headerTitle}>
               <div>
-                <Image src="/logo.png" alt={siteMetadata.headerTitle} width={48} height={48} />
+                <Image src='/images/logo.png' alt={siteMetadata.headerTitle} width={48} height={48} />
               </div>
             </Link>
           </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
+          <div className='flex items-center text-base leading-5'>
+            <div className='hidden sm:block'>
               {headerNavLinks.map((link) => (
                 <Link
                   key={link.title}
                   href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                  className='p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4'
                 >
                   {link.title}
                 </Link>
@@ -35,7 +35,7 @@ const LayoutWrapper = ({ children }) => {
             <MobileNav />
           </div>
         </header>
-        <main className="mb-auto">{children}</main>
+        <main className='mb-auto'>{children}</main>
         <Footer />
       </div>
     </SectionContainer>
