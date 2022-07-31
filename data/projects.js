99f684a138413f5c2f6f273f29c64406bd1b2f26
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-07-31 10:33:55 +0200

    Add new projects

--- data/projects.js
+++ data/projects.js
@@ -13,11 +13,31 @@ const projects = [
     banner: '/images/projects/banners/next-prayer.png',
     title: 'next-prayer',
     description: 'Islamic prayers reminder for your *nix status bar.',
-    tools: ['C++', 'Python', 'Docker'],
-    keywords: ['Prayer Times', 'Status Bar', 'Islamic'],
+    tools: ['C++', 'Python', 'Docker', 'Make'],
+    keywords: ['Prayer Times', 'Status Bar', 'Hijri Date', 'Adhan', 'AUR'],
     source: 'https://github.com/AbdeltwabMF/next-prayer',
     link: 'https://hub.docker.com/r/abdeltwabmf/next-prayer',
     href: '/projects/next-prayer'
+  },
+  {
+    banner: '/images/projects/banners/anthology-of-algorithms-and-data-structures.png',
+    title: 'anthology-of-algorithms-and-data-structures',
+    description: 'Common Code for Competitive Programming in C++',
+    tools: ['C++'],
+    keywords: ['Graph Theory', 'Mathematics', 'Geometry', 'Game Theory', 'Data Structures', 'Ad-hoc', 'String Processing'],
+    source: 'https://github.com/AbdeltwabMF/anthology-of-algorithms-and-data-structures',
+    link: 'https://abdeltwabmf.github.io/anthology-of-algorithms-and-data-structures',
+    href: '/projects/anthology-of-algorithms-and-data-structures'
+  },
+  {
+    banner: '/images/projects/banners/queueing-modelsim.png',
+    title: 'Queueing ModelSim',
+    description: 'A software created for the calculation of characteristics of different queues having the BMAP (batch Markovian Arrival Process) as input, and the deterministic queues characterized by units of input (i.e. customers) arrive at known points in time and the service times are fixed intervals.',
+    tools: ['Python', 'Tkinter'],
+    keywords: ['Queueing Theory', 'BMAP', 'Deterministic Queue', 'Stochastic Queue'],
+    source: 'https://github.com/AbdeltwabMF/queueing-modelsim',
+    link: 'https://abdeltwabmf.github.io/queueing-modelsim',
+    href: '/projects/queueing-modelsim'
   }
 ]
 
