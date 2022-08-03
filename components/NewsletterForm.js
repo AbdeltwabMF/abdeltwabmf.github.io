5764600d625e6f4ff9e2a48b3fac902f85b0e7ef
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-08-03 22:51:26 +0200

    Add newsletter form

--- components/NewsletterForm.js
+++ components/NewsletterForm.js
@@ -13,17 +13,18 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
 
     const res = await fetch(`/api/${siteMetadata.newsletter.provider}`, {
       body: JSON.stringify({
-        email: inputEl.current.value,
+        email: inputEl.current.value
       }),
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      method: 'POST',
+      method: 'POST'
     })
 
     const { error } = await res.json()
     if (error) {
       setError(true)
+      console.log(error)
       setMessage('Your e-mail address is invalid or you are already subscribed!')
       return
     }
@@ -36,30 +37,30 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
 
   return (
     <div>
-      <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100">{title}</div>
-      <form className="flex flex-col sm:flex-row" onSubmit={subscribe}>
+      <div className='pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100'>{title}</div>
+      <form className='flex flex-col sm:flex-row' onSubmit={subscribe}>
         <div>
-          <label className="sr-only" htmlFor="email-input">
+          <label className='sr-only' htmlFor='email-input'>
             Email address
           </label>
           <input
-            autoComplete="email"
-            className="w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
-            id="email-input"
-            name="email"
+            autoComplete='email'
+            className='px-4 w-72 rounded-md focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black'
+            id='email-input'
+            name='email'
             placeholder={subscribed ? "You're subscribed !  🎉" : 'Enter your email'}
             ref={inputEl}
             required
-            type="email"
+            type='email'
             disabled={subscribed}
           />
         </div>
-        <div className="mt-2 flex w-full rounded-md shadow-sm sm:mt-0 sm:ml-3">
+        <div className='flex w-full mt-2 rounded-md shadow-sm sm:mt-0 sm:ml-3'>
           <button
             className={`w-full rounded-md bg-primary-500 py-2 px-4 font-medium text-white sm:py-0 ${
               subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
             } focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black`}
-            type="submit"
+            type='submit'
             disabled={subscribed}
           >
             {subscribed ? 'Thank you!' : 'Sign up'}
@@ -67,7 +68,7 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
         </div>
       </form>
       {error && (
-        <div className="w-72 pt-2 text-sm text-red-500 dark:text-red-400 sm:w-96">{message}</div>
+        <div className='pt-2 text-sm text-red-500 w-72 dark:text-red-400 sm:w-96'>{message}</div>
       )}
     </div>
   )
@@ -76,8 +77,8 @@ const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
 export default NewsletterForm
 
 export const BlogNewsletterForm = ({ title }) => (
-  <div className="flex items-center justify-center">
-    <div className="bg-gray-100 p-6 dark:bg-gray-800 sm:px-14 sm:py-8">
+  <div className='flex items-center justify-center'>
+    <div className='p-6 bg-gray-100 dark:bg-gray-800 sm:px-14 sm:py-8'>
       <NewsletterForm title={title} />
     </div>
   </div>
